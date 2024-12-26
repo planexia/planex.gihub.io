967c35a291d84d79a3328119f8bfd22447e89986
rpruiz33 <rpruiz33@gmail.com>
2024-12-26 20:28:36 -0300

    firs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://back-planexia.onrender.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
